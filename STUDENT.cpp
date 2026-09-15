d02d96cfe0d8fd50c8cd730f6ae9cf4681f8bb99
thithi06071980-pixel <thithi06071980@gmail.com>
2026-09-15 12:04:34 +0700

    Update STUDENT.cpp

--- STUDENT.cpp
+++ STUDENT.cpp
@@ -1,6 +1,7 @@
 #include "STUDENT.h"
 #include <iostream>
 #include <sstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -41,25 +42,25 @@ string Student::getSdt() const{
 }
 
 //setter
-void Student::setMssv(const string mssv) {
+void Student::setMssv(const string& mssv) {
     this->mssv = mssv;
 }
-void Student::setHoten(const string hoten) {
+void Student::setHoten(const string& hoten) {
     this->hoten = hoten;
 }
-void Student::setNgaysinh(const string ngaysinh) {
+void Student::setNgaysinh(const string& ngaysinh) {
     this->ngaysinh = ngaysinh;
 }
-void Student::setGioitinh(const string gioitinh) {
+void Student::setGioitinh(const string& gioitinh) {
     this->gioitinh = gioitinh;
 }
-void Student::setTenlop(const string tenlop) {
+void Student::setTenlop(const string& tenlop) {
     this->tenlop = tenlop;
 }
-void Student::setKhoa(const string khoa) {
+void Student::setKhoa(const string& khoa) {
     this->khoa = khoa;
 }
-void Student::setSdt (const string sdt) {
+void Student::setSdt (const string& sdt) {
 	this->sdt = sdt;
 }
 
@@ -67,27 +68,27 @@ void Student::setSdt (const string sdt) {
 void Student::input() {
 	cout << "Nhap ma sinh vien: "; getline (cin, mssv);
 	cout << "Nhap ho va ten: "; getline (cin, hoten);
-	cout << "Nhap ngay sinh: "; getline (cin, ngaysinh);
-	cout << "Nhap gioi tinh: "; getline (cin, gioitinh);
+	cout << "Nhap ngày sinh: "; getline (cin, ngaysinh);
+	cout << "Nhap giới tinh: "; getline (cin, gioitinh);
 	cout << "Nhap ten lop: "; getline(cin, tenlop);
 	cout << "Nhap khoa: "; getline (cin, khoa);
 	cout << "Nhap so dien thoai: "; getline (cin, sdt);
 }
 
-void SinhVien::xuat() const {
+void Student::output() const {
     cout << left
          << setw(10) << mssv
          << setw(22) << hoten
          << setw(12) << ngaysinh
          << setw(6)  << gioitinh
-         << setw(10) << lop
+         << setw(10) << tenlop
          << setw(15) << khoa
          << setw(13) << sdt
          << "\n";
 }
 
 string Student::toLine() const {
-	return mssv + "|" + hoten + "|" + ngaysinh + "|" + gioitinh + "|" + lop + "|" + khoa  + "|" + sdt;
+	return mssv + "|" + hoten + "|" + ngaysinh + "|" + gioitinh + "|" + tenlop + "|" + khoa  + "|" + sdt;
 }
 
 Student Student::fromLine(const string &line) {
@@ -95,7 +96,7 @@ Student Student::fromLine(const string &line) {
 	spilit(line, '|', datas);
 	Student sv;
 	sv.setMssv(datas[0]);
-	sv.setHoten(datas[1]);
+    sv.setHoten(datas[1]);
 	sv.setNgaysinh(datas[2]);
 	sv.setGioitinh(datas[3]);
 	sv.setTenlop(datas[4]);
